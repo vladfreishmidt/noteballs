8e6ff6300fd615a71b8296f93c47fe6ccae44027
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-03-30 14:49:15 +0300

    Initial setup

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from "vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import App from "./App.vue";
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [],
+});
+
+createApp(App).use(router).mount("#app");
